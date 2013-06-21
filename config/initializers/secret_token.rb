4f9488e3caba0b8cc6aaa60c2eb7aa758c63c6d2
averageazn247 <averageazn1337@gmail.com>
2013-06-21 12:13:10 -0500

    v1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Khoatwiitter::Application.config.secret_token = '94b6bac7a345109cd989f54f11a3b12e3fab04518d160c0b5028b6e29aa4f0c868c8cece2ee54c3f2e92c6ae8a80cb1dc598772526ea681ca27ab6170d23dfd8'
