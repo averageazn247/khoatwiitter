32bcddd28f5c15d024ff104965391387cfed1b9c
averageazn247 <averageazn1337@gmail.com>
2013-06-21 17:49:55 -0500

    added static image

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,7 +19,7 @@ Khoatwiitter::Application.configure do
 
   # Generate digests for assets URLs
   config.assets.digest = true
-
+config.action_dispatch.x_sendfile_header = nil # For Heroku
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
 
